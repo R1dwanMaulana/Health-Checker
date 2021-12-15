3c853a4ef0e1413d7d6f184dae8cca1db97c9f81
Ridwan Maulana <rdwanf49@gmail.com>
2021-12-15 09:12:22 +0700

    chore: add metadata

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,5 +3,34 @@ export default {
   middleware({ store, redirect }) {
     return redirect("/home");
   },
+  head: {
+    title: "Health Check | Aplikasi Identifikasi Penyakit Anemia",
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: "Aplikasi identikasi penyakit anemia",
+      },
+      { property: "og:image", content: "/bot1.png" },
+      { name: "format-detection", content: "telephone=no" },
+      {
+        name: "description",
+        content: "Aplikasi identifikasi penyakit anemia",
+      },
+      {
+        property: "og:title",
+        content: "Health Check | Aplikasi Identifikasi Penyakit Anemia",
+      },
+      {
+        property: "og:description",
+        content: "Aplikasi identifikasi penyakit anemia",
+      },
+      { property: "og:type", content: "website" },
+      { property: "og:site_name", content: "Health Check" },
+      { property: "og:url", content: "https://health-check.vercel.app/" },
+    ],
+  },
 };
 </script>
